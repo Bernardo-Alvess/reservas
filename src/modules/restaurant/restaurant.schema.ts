713cb1f9332c4734bc31f8f72bfa6c5133ea192c
Bernardo <bernardoalves330@gmail.com>
2025-02-27 21:58:52 -0300

    feat: teste lint

--- src/modules/restaurant/restaurant.schema.ts
+++ src/modules/restaurant/restaurant.schema.ts
@@ -1,22 +1,23 @@
-import { Prop, Schema } from '@nestjs/mongoose';
-import { Types } from 'mongoose';
-import { AddressDto } from 'src/common/dto/AddressDto';
-import { WorkHoursDto } from './dto/WorkHoursDto';
+import { Prop, Schema } from "@nestjs/mongoose";
+import { Types } from "mongoose";
+import { AddressDto } from "src/common/dto/AddressDto";
+import { WorkHoursDto } from "./dto/WorkHoursDto";
 
 @Schema({ timestamps: true })
 export class Restaurant {
-    @Prop({ required: true })
-    name: string;
-    @Prop({ required: true })
-    phone: string;
-    @Prop({ type: AddressDto })
-    address: AddressDto;
-    @Prop({ required: true })
-    description: string;
-    @Prop({ required: true })
-    maxClients: number;
-    @Prop({ required: true, type: [WorkHoursDto] })
-    workHours: WorkHoursDto[];
-    @Prop({ type: Types.ObjectId, ref: 'Company' })
-    companyId: Types.ObjectId;
+  @Prop({ required: true })
+  name: string;
+  @Prop({ required: true })
+  phone: string;
+  @Prop({ type: AddressDto })
+  address: AddressDto;
+  @Prop({ required: true })
+  description: string;
+  @Prop({ required: true })
+  maxClients: number;
+  @Prop({ required: true, type: [WorkHoursDto] })
+  workHours: WorkHoursDto[];
+  @Prop({ type: Types.ObjectId, ref: "Company" })
+  companyId: Types.ObjectId;
 }
+//teste

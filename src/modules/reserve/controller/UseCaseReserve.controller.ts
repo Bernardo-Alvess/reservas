ee63db34be07e5527824881160e855f98aa9899b
Bernardo <bernardoalves330@gmail.com>
2025-03-11 17:17:31 -0300

    sec pipeline

--- src/modules/reserve/controller/UseCaseReserve.controller.ts
+++ src/modules/reserve/controller/UseCaseReserve.controller.ts
@@ -18,8 +18,7 @@ export class UseCaseReserveController {
     @Body() reserve: CreateReserveDto,
     @Cookies('sessionToken') sessionToken: string,
   ) {
-    const API_KEY = 'AJFDLKASJFLDKSJA123KJl#44089053280123';
-    console.log(API_KEY);
+    // cloudinary://.
     const payload = await this.userJwtService.checkSessionToken(sessionToken);
     const clientId = payload.sub;
     return this.useCaseReserveService.createReserve(reserve, clientId);

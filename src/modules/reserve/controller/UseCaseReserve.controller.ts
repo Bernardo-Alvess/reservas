479ddf80258d3b59d66ed54c8d323159fd5bb987
Bernardo <bernardoalves330@gmail.com>
2025-03-11 17:24:48 -0300

    test fake secret

--- src/modules/reserve/controller/UseCaseReserve.controller.ts
+++ src/modules/reserve/controller/UseCaseReserve.controller.ts
@@ -18,7 +18,8 @@ export class UseCaseReserveController {
     @Body() reserve: CreateReserveDto,
     @Cookies('sessionToken') sessionToken: string,
   ) {
-    // cloudinary://.
+    const API_KEY = '1234567890abcdef';
+    console.log(API_KEY);
     const payload = await this.userJwtService.checkSessionToken(sessionToken);
     const clientId = payload.sub;
     return this.useCaseReserveService.createReserve(reserve, clientId);
